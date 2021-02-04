5ef6424d039e79c28f82ec636c56a4987a655567
longchaoliu <liujoy57@gmail.com>
2021-02-03 21:23:45 -0800

    Created button function that renders button depending on title and number of notifications

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -6,13 +6,27 @@ import BottomButton from '../components/BottomButton';
 const HomeScreen = ({ navigation }) => {
     return (
         <View style={styles.container}>
-            <Text> Home Page </Text>
-
             <TouchableOpacity style={styles.searchStyle} onPress={() => navigation.navigate('Search')}>
                 <Text style={styles.textStyle}>Search All </Text>
-                <FontAwesome style={styles.arrowStyle} name="arrow-right" />
+                <FontAwesome style={styles.arrowStyle} name="search" />
             </TouchableOpacity>
-
+            <View style={styles.row}>
+                <Button title="Articles" notifications={1}></Button>
+                <Button title="Podcasts" notifications={3} color = "#6EC6B3"></Button>
+            </View> 
+            <View style={styles.row}>
+                <Button title="Blogs" notifications={1} color="#C5DB65" ></Button>
+                <Button title="Career Advice" notifications={1} ></Button>
+                <Button title="Videos" notifications={1} color="#C5DB65" ></Button>
+            </View>
+            <View style={styles.row}>
+                <Button title="Events" notifications={2} color="#F7F6F1"></Button>
+                <Button title="Authenticity Meter" notifications={2} color="#6EC6B3"></Button>
+            </View>
+            <View style={styles.row}>
+                <Button title="Guides" notifications={2}></Button>
+                <Button title="Resources" notifications={1} color="#C5DB65"></Button>
+            </View>
             <BottomButton navigation={navigation} />
 
         </View>
@@ -34,9 +48,42 @@ HomeScreen.navigationOptions = ({ navigation }) => {
     };
 };
 
+function Button(props) {
+    const notifs = Math.min(props.notifications, 3);
+
+    return <TouchableOpacity style={[styles.button, {
+        backgroundColor: props.color == null ? "#1B8AE6" : props.color, 
+        borderRadius: 25+ notifs*25,
+        width: 50+ notifs *50,
+        height: 50+ notifs *50,
+    }]}> 
+        <Text style={{
+            fontSize: props.title.length > Math.max(5*notifs, 10) ? 5+ 10* notifs: 10 + notifs * 10,
+            textAlign: "center"
+        }}> {props.title}</Text>
+            </TouchableOpacity>;
+}
+
 const styles = StyleSheet.create({
     container: {
         flex: 1
+    },
+    button: {
+        borderRadius: 50,
+        width:100,
+        height:100,
+        alignItems:'center',
+        justifyContent: 'center',
+        shadowColor: 'rgba(0,0,0, .4)', 
+        shadowOffset: { height: 4, width: 4 }, 
+        shadowOpacity: 1, 
+        shadowRadius: 1,
+        margin: 10
+    },
+    row: {
+        flexDirection: "row", 
+        alignItems:'center',
+        justifyContent: 'center',
     },
     icon: {
         flex: 1,
