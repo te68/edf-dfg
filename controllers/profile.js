209a88d05acb88bddba1015f138b731a49f91dc7
Yazan <me@yazanalmatar.com>
2021-02-23 14:26:10 -0500

    added getProfileById controller

--- controllers/profile.js
+++ controllers/profile.js
@@ -80,3 +80,20 @@ exports.createUpdateProfile =
     }
     res.send(profileFields);
   });
+
+exports.getProfileById = async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("user", ["name", "avatar"]);
+    if (!profile) return res.status(400).json({ msg: "Profile Not Found" });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(400).json({ msg: "Profile Not Found" });
+    }
+    res.status(500).send("Server Error");
+  }
+};
