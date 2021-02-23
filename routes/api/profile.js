e62443bb2ef647e6bc78fc1b6740e49733f6ee56
Yazan <me@yazanalmatar.com>
2021-02-23 14:26:36 -0500

    added auth to route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -8,11 +8,15 @@ const router = express.Router();
 // @route  GET api/profile/me
 // @desc   Get current users profile
 // @access Private
-router.get("/me", profileControllers.getProfile);
+router.get("/me", auth, profileControllers.getProfile);
 
 // @route  POST api/profile/profile
 // @desc   Create or update a user Profile
 // @access Private
 router.post("/", auth, profileControllers.createUpdateProfile);
 
+// @route  GET api/profile/user/:user_id
+// @desc   Get profile by user ID
+// @access Public
+router.get("/user/:user_id", profileControllers.getProfileById);
 module.exports = router;
