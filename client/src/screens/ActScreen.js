053ad99a26f87bdafd19f884556b480c6492d375
longchaoliu <liujoy57@gmail.com>
2021-02-25 14:51:45 -0800

    ActScreen

--- client/src/screens/ActScreen.js
+++ client/src/screens/ActScreen.js
@@ -1,14 +1,45 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import BottomButton from '../components/BottomButton';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    Image,
+    ScrollView
+} from 'react-native';
+import {
+    Ionicons,
+    Entypo
+} from '@expo/vector-icons';
+
 
 const ActScreen = ({ navigation }) => {
+    const actions = [
+        { key: 0, title: "Influence Your Employer" },
+        { key: 1, title: "Influence a Future Employer" },
+        { key: 2, title: "Write a Letter to the Editor" },
+        { key: 3, title: "Tell Your Story" },
+        { key: 4, title: "Support Petitions" },
+        { key: 5, title: "Train With Us" }
+    ]
+
     return (
         <View style={styles.container}>
-            <Text> Act Page </Text>
+            <Text style={styles.title}> What Can I Do? </Text>
+            <Text style={styles.heading}> How to: </Text>
+            <ScrollView contentContainerStyle={{ alignItems: 'center' }}>
+                <View>
+                    {actions.map((action) => (
+                        <TouchableOpacity style={styles.act}>
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                                <Text style={styles.body}> {action.title} </Text>
+                                <Entypo name="chevron-right" size={30} color="black" />
+                            </View>
+                        </TouchableOpacity>
+                    ))}
 
-            <BottomButton navigation={navigation} />
+                </View>
+            </ScrollView>
 
         </View>
     );
@@ -31,7 +62,36 @@ ActScreen.navigationOptions = ({ navigation }) => {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        margin: 20
+    },
+    title: {
+        justifyContent: 'center',
+        fontSize: 35,
+        fontWeight: 'bold',
+        paddingBottom: 20,
+    },
+    heading: {
+        fontSize: 25,
+        fontWeight: 'bold',
+    },
+    act: {
+        backgroundColor: "white",
+        borderRadius: 10,
+        width: 360,
+        height: 50,
+        shadowColor: 'rgba(0,0,0, .4)',
+        shadowOffset: { height: 4, width: 4 },
+        shadowOpacity: 1,
+        shadowRadius: 1,
+        justifyContent: 'center',
+        alignContent: 'center',
+        padding: 10,
+        margin: 15,
+    },
+    body: {
+        fontSize: 22,
+        fontWeight: 'bold',
     },
     icon: {
         flex: 1,
