8d7dc9c6d4df73a566130fa4b7d7a79ac04bd738
Yazan <me@yazanalmatar.com>
2021-03-12 01:35:13 -0500

    uncommented route

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "portal/build")));
 
   // Handle React routing, return all requests to the front-end
-  app.get("/admin", function (req, res) {
+  app.get("/admin/*", function (req, res) {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
