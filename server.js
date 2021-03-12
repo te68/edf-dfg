ce719822224de57c41e4cef1106c32c239483a10
Yazan <me@yazanalmatar.com>
2021-03-12 01:22:11 -0500

    ignore

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ if (process.env.NODE_ENV === "production") {
   // Serve any static files
   app.use(express.static(path.join(__dirname, "portal/build")));
 
-  // Handle React routing, return all requests to React app
+  // Handle React routing, return all requests to the front-end
   app.get("/admin", function (req, res) {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
