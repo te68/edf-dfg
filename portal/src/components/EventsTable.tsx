5ced4ec6201da4ea60ffe579b9cf947f96f8b60e
Yazan <me@yazanalmatar.com>
2021-03-11 19:05:27 -0500

    dashboard page

---
+++ portal/src/components/EventsTable.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+
+const EventsTable: React.FC = () => {
+  useEffect(() => {
+    axios
+      .get("https://youth-activism-app-server.herokuapp.com/api/event", {
+        headers: {
+          "Content-Type": "application/json",
+          "x-auth-token": localStorage.getItem("token"),
+        },
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+  return (
+    <section className="section" id="table">
+      <h1 className="title">Events</h1>
+      <hr />
+      <table className="table">
+        <thead>
+          <tr>
+            <th>
+              <a>Title</a>
+            </th>
+            <th>Time</th>
+            <th>address</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+      </table>
+    </section>
+  );
+};
+
+export default EventsTable;
