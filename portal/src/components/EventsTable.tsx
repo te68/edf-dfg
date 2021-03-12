3327ce747cc47e1e64955f9e6f2350312a152354
Yazan <me@yazanalmatar.com>
2021-03-12 02:41:44 -0500

    cors

--- portal/src/components/EventsTable.tsx
+++ portal/src/components/EventsTable.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 const EventsTable: React.FC = () => {
   useEffect(() => {
     axios
-      .get("http://localhost:3000/api/event", {
+      .get("https://youth-activism-app-server.herokuapp.com/api/event", {
         headers: {
           "Content-Type": "application/json",
           "x-auth-token": localStorage.getItem("token"),
