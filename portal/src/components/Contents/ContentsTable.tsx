e61216c7485f241cac39a8db341c372736ff6f0c
Yazan <me@yazanalmatar.com>
2021-03-15 22:41:54 -0400

    initial

---
+++ portal/src/components/Contents/ContentsTable.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ContentTableRow from "./ContentTableRow";
+
+const ContentTable: React.FC = () => {
+  const [content, setContent] = useState<any[]>([]);
+  useEffect(() => {
+    fetchContent();
+  }, []);
+  const fetchContent = () => {
+    axios.get("/api/content", {}).then(async (response) => {
+      await setContent(response.data.content);
+    });
+    console.log(content);
+  };
+  const renderContent = () => {
+    if (!content.length) return <h1>Loading</h1>;
+
+    const renderedEvents = content.map((content) => {
+      return <ContentTableRow content={content} key={content._id} />;
+    });
+    return renderedEvents;
+  };
+  const newContentClick = async () => {
+    await axios.post("/api/content", {
+      title: "New Content",
+      category: "Blog",
+      preview: "Write Preview here",
+      author: "Author's Name",
+      interest: "write interest here",
+      url: "https://www.example.com",
+      featured: false,
+    });
+    fetchContent();
+  };
+  return (
+    <div>
+      <section className="section" id="table">
+        <h1 className="title">Content</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>
+                <a>Title</a>
+              </th>
+              <th>Author</th>
+              <th>Category</th>
+              <th>Featured</th>
+              <th>Edit</th>
+            </tr>
+          </thead>
+          <tfoot>
+            <tr>
+              <tr>
+                <button className="button is-success" onClick={newContentClick}>
+                  + New Content
+                </button>
+              </tr>
+            </tr>
+          </tfoot>
+          <tbody>{renderContent()}</tbody>
+        </table>
+      </section>
+    </div>
+  );
+};
+
+export default ContentTable;
